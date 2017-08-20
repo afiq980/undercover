60310778bb84135712874c3f07fd41411eb2b2b4
Intelicopter <intelicopter@gmail.com>
2017-08-20 13:07:35 +0800

    Initial Commit 2

---
+++ models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+
+class Season(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=25)
+    from_date = models.DateField()
+    to_date = models.DateField()
+
+    class Meta:
+        app_label = 'undercover'
+
+
+class Pair(models.Model):
+    id = models.AutoField(primary_key=True)
+    word1 = models.CharField(max_length=20)
+    word2 = models.CharField(max_length=20)
+    level = models.IntegerField(default=0)
+    season = models.ForeignKey(Season, on_delete=models.CASCADE, null=True, default=None)
+
+    class Meta:
+        app_label = 'undercover'
+
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    email = models.CharField(max_length=50, primary_key=True)
+
+    class Meta:
+        app_label = 'undercover'
+
+
+class UserPair(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User)
+    pair = models.ForeignKey(Pair)
+
+    class Meta:
+        app_label = 'undercover'
+
+
+class Namelist(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, unique=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=14)
+
+    class Meta:
+        app_label = 'undercover'
