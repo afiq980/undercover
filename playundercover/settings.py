da671ec7bc9591e2644ca8e858b6177a7a226d81
Intelicopter <intelicopter@gmail.com>
2017-08-20 14:07:20 +0800

    edit

--- playundercover/settings.py
+++ playundercover/settings.py
@@ -82,24 +82,24 @@ WSGI_APPLICATION = 'playundercover.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dee81bb9uap89n',
-        'USER': 'jelabiiksabbkj',
-        'PASSWORD': 'c7c67c82b6edaf85a4593c88669d6995dd2b14d1ea9454a30a87e6b0723ab937',
-        'HOST': 'ec2-50-19-219-69.compute-1.amazonaws.com',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'dee81bb9uap89n',
+#         'USER': 'jelabiiksabbkj',
+#         'PASSWORD': 'c7c67c82b6edaf85a4593c88669d6995dd2b14d1ea9454a30a87e6b0723ab937',
+#         'HOST': 'ec2-50-19-219-69.compute-1.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
 
 
 AUTH_PASSWORD_VALIDATORS = [
